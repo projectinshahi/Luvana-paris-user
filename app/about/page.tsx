5459908e18ef2db0af8365420a186db6df4cffde
marshook ali <marshookali98@gmail.com>
2026-09-17 09:11:18 +0530

    add a comment for re-push

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import AboutView from "@/components/AboutView";
 
+// About Page
+
 export const metadata: Metadata = {
   title: "About Us | LuvanaParis",
   description:
